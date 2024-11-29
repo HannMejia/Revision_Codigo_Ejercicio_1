6e3924ab5fb9ce89b38d73b4211500f309a932b0
HannMejia <hmargarita.mejiar@gmail.com>
2024-11-29 11:23:10 -0600

    Revisión de código Ejercicio 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog'); //se corrige la llamada al elemento con clase "blog", utilizando . en lugar de #
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //se está trabajando con una función asíncrona, se corrige la declaración de la función con async funtion
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try { //Se implementa el manejo de errores utilizando try/catch
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); //Faltaba declarar una constante data que esperara una respuesta en formato JSON
+    console.log(data);
+    $n.textContent = data.name; //Se corrige el formato 
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (err) { //aquí se implementa el manejo de errores
+    handleError(err);
+  }
+  
 }
 
 function handleError(err) {
